e9a8574215b7bb17aadef006c6428a4f399288ac
Burdaev Evgeniy <hate2me@yandex.ru>
2016-03-22 17:14:26 +0300

    delete hardcoded module name from signed_response_parser.rb

--- lib/yandex_kassa/signed_response_parser.rb
+++ lib/yandex_kassa/signed_response_parser.rb
@@ -3,7 +3,7 @@ module YandexKassa
     def initialize(params = {})
       cert_file = params.fetch(:cert_file)
       @cert_store = OpenSSL::X509::Store.new
-      @cert_store.add_cert(YandexKassa.cert_file)
+      @cert_store.add_cert(cert_file)
 
       @deposit_cert_file = params.fetch(:deposit_cert_file)
     end
