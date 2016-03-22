29ffb4d9f7666682875b9efea7aa5fe43c7b9a67
Burdaev Evgeniy <hate2me@yandex.ru>
2016-03-22 16:49:45 +0300

    place client code into Api class

--- lib/yandex_kassa/api.rb
+++ lib/yandex_kassa/api.rb
@@ -8,18 +8,18 @@ module YandexKassa
       @cert_file = params.fetch(:cert_file)
       @key_file = params.fetch(:key_file)
     end
-  end
 
-  private
+      private
 
-  attr_reader :cert_file, :key_file, :url, :response_parser
+      attr_reader :cert_file, :key_file, :url, :response_parser
 
-  def client
-    @client ||= RestClient::Resource.new(
-      url,
-      ssl_client_cert: cert_file,
-      ssl_client_key: key_file,
-      verify_ssl: OpenSSL::SSL::VERIFY_NONE,
-      headers: { content_type: 'application/pkcs7-mime', content_lenght: '512' })
-  end
+      def client
+        @client ||= RestClient::Resource.new(
+          url,
+          ssl_client_cert: cert_file,
+          ssl_client_key: key_file,
+          verify_ssl: OpenSSL::SSL::VERIFY_NONE,
+          headers: { content_type: 'application/pkcs7-mime', content_lenght: '512' })
+      end
+    end
 end
